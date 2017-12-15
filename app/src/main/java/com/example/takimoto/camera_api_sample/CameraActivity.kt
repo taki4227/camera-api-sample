f685e2350ab8a9984b9fa26549f71110e7cd9c37
takimoto <p277036@cmlabs.jp>
2017-12-15 19:02:48 +0900

    カメラ関連のManifest設定追加

---
+++ app/src/main/java/com/example/takimoto/camera_api_sample/CameraActivity.kt
@@ -0,0 +1,10 @@
+package com.example.takimoto.camera_api_sample
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by takimoto on 2017/12/15.
+ */
+class CameraActivity : AppCompatActivity() {
+
+}
